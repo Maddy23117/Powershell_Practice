62df1f2360b99d1cf7209afdd26c84f3a4152466
Mahesh Pawar <Maddy23117@users.noreply.github.com>
2017-02-24 13:11:59 +0530

    Domain User Local admin right
    Domain User Local admin right

---
+++ Domain User Local admin right.ps1
@@ -0,0 +1,19 @@
+﻿$servers= get-content 'D:\UserData\Desktop\Powershell Practice\ServerList.txt' | Format-Table
+$output = 'D:\UserData\Desktop\Powershell Practice\test.csv'
+$results = @()
+
+foreach($server in $servers)
+{
+$admins = @()
+$group =[ADSI]"WinNT://$server/Administrators" 
+$members = @($group.psbase.Invoke("Members"))
+$members | foreach {
+ $obj = new-object psobject -Property @{
+ Server = $Server
+ Admin = $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+ }
+ $admins += $obj
+ } 
+$results += $admins
+}
+$results| Export-csv $Output -NoTypeInformation
