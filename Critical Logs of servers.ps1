8f8c056e49c7379b5535086123619aec63856ff4
Mahesh Pawar <Mahesh.pawar@edenred.com>
2017-02-24 12:55:07 +0530

    Please Run on Your Own risk.
    Thanks and Regards,
    Mahesh Pawar 
    IT :)

---
+++ Critical Logs of servers.ps1
@@ -0,0 +1,10 @@
+﻿ ## Thank you for Downloading ##
+ ## Tested and used please use Administrator credentials for results.##
+ ##$$ Thanks & Regards, Mahesh Pawar (IT) $$##
+$logs = "Application", "System"
+$yesterday = (get-date) - (New-TimeSpan -day 1)
+$s = "C:\ServerList.txt"
+foreach ($server in Get-Content $s)
+   {$server; get-winevent -logname System -computername $server | where {$_.LevelDisplayName -eq "Critical"}}
+  
+   
